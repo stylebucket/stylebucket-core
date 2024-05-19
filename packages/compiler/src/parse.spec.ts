674f7152ec22edf12fcbd3362093fd150acd709e
Brett Wood <brett@woodbrettm.com>
2024-05-19 05:00:24 -0700

    [IMP] Add Char Escape Test Case to Compiler

--- packages/compiler/src/parse.spec.ts
+++ packages/compiler/src/parse.spec.ts
@@ -1,20 +1,27 @@
+import { css } from '@stylebucket/css';
+
 import { test, expect } from 'vitest';
 import { parseCss } from './parse';
 
 test('ParseCss With PostCSS Prop', () => {
 
-  const css = `
+  const styles = css`
     @media all and (min-width: 1200px) {
       .my-class {
         display: somePostCssFunction();
+      }
+      .\@root {
+        font-size: 12px;
       }
     }
   `;
 
   const exp = (''
-    + '@media all and (min-width: 1200px)'
-    + '{.my-class{display:somePostCssFunction()}}'
+    + '@media all and (min-width: 1200px){'
+    + '.my-class{display:somePostCssFunction()}'
+    + '.\\@root{font-size:12px}'
+    + '}'
   );
 
-  expect(parseCss(css).css).toBe(exp);
+  expect(parseCss(styles).css).toBe(exp);
 });
