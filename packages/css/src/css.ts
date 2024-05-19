120cca601cd1a51845f1aee788ca166ea549d030
Brett Wood <brett@woodbrettm.com>
2024-05-19 03:58:15 -0700

    [FIX] Change CSS Function to Parsing Raw Unescaped Strings

--- packages/css/src/css.ts
+++ packages/css/src/css.ts
@@ -5,24 +5,5 @@ export function css
 )
 : string
 {
-  if (!Array.isArray(strings)) return '';
-  if (!strings.length) return '';
-
-  /*
-    String array is always 1 length longer than interpolations array
-    e.g. string, interp, string, interp, string, interp, string.
-    Always begins and ends with string.
-  */
-
-  let cssStr = '';
-  let index = 0;
-  cssStr += strings[0];
-
-  for (const interpolation of interpolations) {
-    cssStr += interpolation.toString();
-    index += 1;
-    cssStr += strings[index] ? strings[index] : '';
-  }
-
-  return cssStr;
+  return String.raw(strings, ...interpolations);
 }
